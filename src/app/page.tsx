984a33e2148fd46523f84e9e7b10b5ff006980fa
borin98 <borinmacedo@gmail.com>
2024-06-02 11:33:56 -0300

    feat(Home-page): Include the initial template for section homepage
    Author: Gabriel Borin Macedo (github : borin98)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import "@/styles/globals.css"
+import Image from "next/image";
+import CafeSection from "@/public/cafeSection.png"
 
+/**
+ * Renders the root layout of the application.
+ * @TODO : Use a better font for the page
+ * @TODO : Move to another component the header and the main page content
+ * @returns {JSX.Element} The root layout component.
+ */
 export default function Home() {
     return (
-        <main>
-            <h1>Hello word !!!</h1>
+        <main className={"w-full flex flex-row pl-40"}>
+            <section className={"flex flex-row gap-x-20 w-full"}>
+                <div className={"flex flex-col gap-y-3 w-max-[588px]"}>
+                    <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                    <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                </div>
+                <Image className={"ml-40"} src={CafeSection} alt={""} width={360} height={476}/>
+            </section>
         </main>
     );
 }
