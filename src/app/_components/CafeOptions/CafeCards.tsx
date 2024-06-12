844beb02a75b7d9a4038a7d46308a5dfc1f97ffd
borin98 <borinmacedo@gmail.com>
2024-06-12 20:30:51 -0300

    feat(cafe-content): Add badge, input and button to add the cafe quantity to the cart
    Include two news shadcn components : badge and input and also add a styled button to café card
    
    Author: Gabriel Borin Macedo (github : borin98)

--- src/app/_components/CafeOptions/CafeCards.tsx
+++ src/app/_components/CafeOptions/CafeCards.tsx
@@ -1,21 +1,34 @@
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Badge} from "@/components/ui/badge";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
 
 export function CafeCards() {
     return (
         <Card className={"w-64 h-[310px] flex flex-col items-center justify-center shadow-xl"}>
             <CardHeader className={"flex flex-col gap-y-2"}>
                 <span>Img 1</span>
-                <span>Categoria</span>
+                <Badge>Categoria</Badge>
             </CardHeader>
             <CardContent className={"flex flex-col gap-y-2"}>
                 <CardTitle> Café do Brasil</CardTitle>
                 <CardDescription>Descrição</CardDescription>
             </CardContent>
-            <CardFooter className={"flex justify-between"}>
-                <span>R$ 100</span>
-                <div className={"flex flex-row gap-x-2"}>
-                    <span>+-</span>
-                    <span>carrinho</span>
+            <CardFooter className={"flex flex-row gap-x-2"}>
+                <div
+                    className={"inline-flex items-center rounded-lg bg-white border border-gray-200 dark:border-gray-800"}>
+                    <Button variant={"ghost"}
+                            className={"rounded-l-lg px-3 py-2 text-purple-rocket hover:bg-gray-100 dark:hover:bg-gray-800"}>
+                        -
+                    </Button>
+                    <span className={"mx-2 text-sm font-medium"}>
+                        0
+                    </span>
+                    <Button
+                        variant={"ghost"}
+                        className={"rounded-r-lg px-3 py-2 text-purple-rocket hover:bg-gray-100 dark:hover:bg-gray-800"}>
+                        +
+                    </Button>
                 </div>
             </CardFooter>
         </Card>
